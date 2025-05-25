76ff50cc252aeb5525256421383968b80c3ced89
jagdishlove <jagdish.funn@gmail.com>
2025-05-26 00:38:27 +0100

    updated changes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,3 @@
-import WidgetWebComponent from "@/web-component";
+import WidgetWebComponent from "./web-component";
 
 customElements.define("my-widget", WidgetWebComponent);
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   define: {
     "process.env": {
       NODE_ENV: "production",
     },
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -18,9 +17,9 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: "./src/index.tsx",
+      entry: "./src/index.jsx",
       name: "widget",
-      fileName: (format) => `widget.${format}.ts`,
+      fileName: (format) => `widget.${format}.js`,
     },
     target: "esnext",
   },
